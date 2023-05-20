668fbc8819f866c3f66ea1c828b8223c15fd2008
EDJR94 <edilson07.santos1@gmail.com>
2023-05-20 10:39:16 -0300

    -----

--- api/handler.py
+++ api/handler.py
@@ -3,50 +3,45 @@ import pandas as pd
 from flask import Flask, request, Response
 from healthinsurance.HealthInsurance import HealthInsurance
 
-#loading model
-path = 'C:/Users/edils/repos/pa004_health_insurance/'
-model = pickle.load(open(path + 'src/models/model_lgb.pkl','rb'))
+# loading model
+model = pickle.load( open( 'C:/Users/edils/repos/pa004_health_insurance/src/models/model_lgb.pkl', 'rb') )
 
-#Initiate API
-app = Flask(__name__)
+# initialize API
+app = Flask( __name__ )
 
-@app.route('/healthinsurance/predict', methods=['POST'])
-
+@app.route( '/healthinsurance/predict', methods=['POST'] )
 def healthinsurance_predict():
-    
-    test_json = request.get_json() #Load json for production
-    
-    if test_json: #there is data
-        if isinstance(test_json, dict ): #only one row
-            test_raw = pd.DataFrame(test_json, index=[0]) #keys for dict transform in columns
-        
-        else: #has multiple rows
-            test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
-
-        #Instatiate Class
+    test_json = request.get_json()
+   
+    if test_json: # there is data
+        if isinstance( test_json, dict ): # unique example
+            test_raw = pd.DataFrame( test_json, index=[0] )
+            
+        else: # multiple example
+            test_raw = pd.DataFrame( test_json, columns=test_json[0].keys() )
+            
+        # Instantiate Rossmann class
         pipeline = HealthInsurance()
-
-        #Data Description
-        df1 = pipeline.rename_columns(test_raw)
-
-        #Data Transformation
-        df2 = pipeline.rename_categorical(df1)
-
-        #Data Encoding
-        df3 = pipeline.data_encoding(df2)
         
-        #Data Rescalling
+        # data cleaning
+        df1 = pipeline.rename_columns( test_raw )
+        
+        # feature engineering
+        df2 = pipeline.rename_categorical( df1 )
+        
+        # data preparation
+        df3 = pipeline.data_encoding( df2 )
+        
         df4 = pipeline.data_rescalling(df3)
-
-        #prediction
-        df_response = pipeline.get_prediction(model, test_raw, df4)
         
-        print(df4)
-
+        # prediction
+        df_response = pipeline.get_prediction( model, test_raw, df4 )
+        
         return df_response
-    
+        
+        
     else:
-        return Response('{}', status=200, mimetype='application/json')
+        return Response( '{}', status=200, mimetype='application/json' )
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', debug=True)
+    app.run( '0.0.0.0' )
